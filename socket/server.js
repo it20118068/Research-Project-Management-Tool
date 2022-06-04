60e85615d2192f55121791ff515e8709aef840f8
it20118068 <89683180+it20118068@users.noreply.github.com>
2022-06-04 12:07:06 +0530

    Chat Socket

---
+++ socket/server.js
@@ -0,0 +1,52 @@
+
+require("dotenv").config();
+
+
+const PORT = process.env.PORT || 8900;
+
+const io = require("socket.io")(PORT, {
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
+  
+  let users = [];
+  
+  const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+      users.push({ userId, socketId });
+  };
+  
+  const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+  };
+  
+  const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+  };
+  
+  io.on("connection", (socket) => {
+    //when ceonnect
+    console.log("a user connected.");
+  
+    //take userId and socketId from user
+    socket.on("addUser", (userId) => {
+      addUser(userId, socket.id);
+      io.emit("getUsers", users);
+    });
+  
+    //send and get message
+    socket.on("sendMessage", ({ message }) => {
+      console.log(message)
+      io.emit("getMessage", {
+        message
+      });
+    });
+  
+    //when disconnect
+    socket.on("disconnect", () => {
+      console.log("a user disconnected!");
+      removeUser(socket.id);
+      io.emit("getUsers", users);
+    });
+  });
